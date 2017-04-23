05769c3b4563a3cbfa3c63de6fad14f8dd2784ba
Ekaterina Pyataeva <katya@mail.ru>
2017-04-23 12:47:16 +0500

    Init commit

---
+++ src/main/java/com/demo/DemoProgramsApp.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ekaterina on 23.04.2017.
+ */
+
+@SpringBootApplication
+public class DemoProgramsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProgramsApp.class, args);
+    }
+}
